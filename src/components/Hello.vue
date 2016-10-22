2f029be6b129fde712ac88260ae94971dcd233d7
Keith Wade <keawade@gmail.com>
2016-10-21 19:27:09 -0500

    Implemented vue-resource

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <button v-on:click='getThing'>Get Thing</button>
   </div>
 </template>
 
@@ -17,6 +11,17 @@ export default {
   data () {
     return {
       msg: 'Welcome to Garden View'
+    }
+  },
+  methods: {
+    getThing () {
+      this.$http.get('http://localhost:3000/api')
+        .then((res) => {
+          console.info('got thing!', res.body.msg)
+        }, (error) => {
+          console.error('failed to get resource', error)
+          return 'error'
+        })
     }
   }
 }
