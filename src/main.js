0ee5747afefb25e5d7ec37461091144b7daaaf86
Keith Wade <keawade@gmail.com>
2016-11-02 07:32:40 -0500

    Add Vuex for handling auth

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 // import App from './App'
 import App from './App'
@@ -14,6 +16,19 @@ import Login from './components/Login'
 
 Vue.http.options.root = 'http://garden-api.herokuapp.com'
 
+const requireAuth = (to, from, next) => {
+  if (!auth.loggedIn()) {
+    next({
+      path: '/login',
+      query: {
+        redirect: to.fullPath
+      }
+    })
+  } else {
+    next()
+  }
+}
+
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
@@ -22,19 +37,23 @@ const router = new VueRouter({
     component: App,
     children: [{
       path: '',
-      component: Feed
+      component: Feed,
+      beforeEnter: requireAuth
     }, {
       path: 'following',
-      component: Following
+      component: Following,
+      beforeEnter: requireAuth
     }, {
       path: 'profile',
-      component: Profile
+      component: Profile,
+      beforeEnter: requireAuth
     }, {
       path: '/login',
       component: Login
     }, {
       path: '/logout',
       beforeEnter (to, from, next) {
+        auth.logout()
         next('/login')
       }
     }]
@@ -44,7 +63,74 @@ const router = new VueRouter({
   }]
 })
 
+const authRequest = (user, pass, cb) => {
+  console.log('auth request')
+}
+
+const auth = {
+  login (user, pass, cb) {
+    cb = arguments[arguments.length - 1]
+    if (store.state.token) {
+      if (cb) cb(true)
+      this.onChange(true)
+      return
+    }
+    authRequest(user, pass, (res) => {
+      if (res.authenticated) {
+        store.commit('setToken', res.token)
+        store.commit('setUser', user)
+        store.commit('setIsAuthenticated', true)
+        if (cb) cb(true)
+        this.onChange(true)
+      } else {
+        if (cb) cb(false)
+        store.commit('setIsAuthenticated', false)
+        this.onChange(false)
+      }
+    })
+  },
+
+  getToken () {
+    return store.state.token
+  },
+
+  logout (cb) {
+    store.commit('setToken', '')
+    store.commit('setUser', {})
+    store.commit('setIsAuthenticated', false)
+    if (cb) cb()
+    this.onChange(false)
+  },
+
+  loggedIn () {
+    return !!store.state.token
+  },
+
+  onChange () {}
+}
+
 /* eslint-disable no-new */
+const store = new Vuex.Store({
+  state: {
+    token: '',
+    user: {},
+    isAuthenticated: false
+  },
+  mutations: {
+    setToken (state, token) {
+      state.token = token
+    },
+    setIsAuthenticated (state, toggle) {
+      state.isAuthenticated = toggle
+    },
+    setUser (state, user) {
+      state.user = user
+    }
+  }
+})
+
 new Vue({
-  router
+  router,
+  store,
+  auth
 }).$mount('#app')
