518299244ab0a6d5c19e2e8a4b9e59dbf9810e7a
Keith Wade <keawade@gmail.com>
2016-11-03 19:46:13 -0500

    Update users with cards

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,9 +1,19 @@
 <template>
-	<div class='ui four column stackable grid container top-padder'>
-    <div v-for='user in users' class='column'>
-		  <user-item :user='user' />
+  <div class='ui container top-padder'>
+    <div class='ui four cards'>
+      <div v-for='user in users' class='card'>
+        <div class='content'>
+          <div class='header'>{{ user.username }}</div>
+          <div class='meta'>{{ user.username }}</div>
+          <div class='description'>{{ user.username }}</div>
+        </div>
+        <div class='extra content'>
+          <button class='fluid ui basic secondary button' v-on:click='unfollow(user)' v-if='isFollowing(user)'>Unfollow</button>
+          <button class='fluid ui basic primary button' v-on:click='follow(user)' v-else>Follow</button>
+        </div>
+      </div>
     </div>
-	</div>
+  </div>
 </template>
 <script>
   import UserItem from './UserItem'
@@ -22,12 +32,38 @@
         }).filter((user) => {
           return user !== undefined
         })
+        console.info('users', this.users)
       }, (res) => {
         console.log('[Users] failed to get users', res)
       })
     },
     components: {
       UserItem
+    },
+    methods: {
+      isFollowing (user) {
+        return this.$store.state.user.following.indexOf(user._id) > -1
+      },
+      follow (user) {
+        console.log('[UserItem] following', user.username)
+        this.$http.post(`user/${user.username}/follow`, {
+          token: this.$store.state.token
+        }).then((res) => {
+          this.$store.commit('setUser', res.body)
+        }, (res) => {
+          console.error('[UserItem] failed to unfollow user', res.body)
+        })
+      },
+      unfollow (user) {
+        console.log('[UserItem] unfollowing', user.username)
+        this.$http.put(`user/${user.username}/unfollow`, {
+          token: this.$store.state.token
+        }).then((res) => {
+          this.$store.commit('setUser', res.body)
+        }, (res) => {
+          console.error('[UserItem] failed to unfollow user', res.body)
+        })
+      }
     }
   }
 </script>
