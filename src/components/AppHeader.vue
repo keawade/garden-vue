257bc731843678200668e4dc41978809656bca15
Keith Wade <keawade@gmail.com>
2016-11-03 20:38:37 -0500

    Fix isAuthenticated hook on header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,21 +7,14 @@
         <router-link to='/users' class='item'>Find Users</router-link>
         <div class='right menu'>
           <router-link to='/profile' class='item'>Profile</router-link>
-          <router-link v-if='!isAuthenticated' to='/logout' class='item'>Logout</router-link>
-          <router-link v-if='isAuthenticated' to='/login' class='item'>Login</router-link>
+          <router-link v-if='this.$store.state.isAuthenticated' to='/logout' class='item'>Logout</router-link>
+          <router-link v-else to='/login' class='item'>Login</router-link>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        isAuthenticated: this.$store.isAuthenticated
-      }
-    }
-  }
 </script>
 <style scoped>
 
