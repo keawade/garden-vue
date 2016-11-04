713c044c7b11fc3df1e9cc737622558ee31ade97
Keith Wade <keawade@gmail.com>
2016-11-03 19:47:52 -0500

    And remove component

--- src/components/Following.vue
+++ src/components/Following.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class='ui four column stackable grid container top-padder'>
+	<div class='ui four cards top-padder'>
 		<div class='column'>
 			<div class='ui segment'>
 				<img class='ui medium image' src='/static/media-paragraph.png' />
